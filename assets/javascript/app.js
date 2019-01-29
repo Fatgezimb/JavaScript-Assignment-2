1bf9049b214a8bafe10ef460de9f1fd0ac10cc93
Fatgezim Bela <fbela@gmu.edu>
2019-01-28 20:16:59 -0500

    Layout change
    need to add more jQuery

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,27 @@
-console.log('Node is installed!');
-
-
-$(document).ready(function () {
-    $("#accordian").accordion( {
-        
-    })
+// creating variables 
+var quiz = $("#quiz-area");
+var timeLaps = 20;
 
 
 
+// handle click events
+$("")
+
+// press start -> start timer -> show question
+
+// Timer question
+
+// Show 1st question function
+
+
+// Determine if wrong or right
+
+
+// Time delay 
+
+
+// Show next question + Start timer
 
 
 
 
-
-
-
-
-
-
-
-
-});
